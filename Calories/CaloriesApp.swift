f5e99ed0c1dec738c53b162b739bcb6e83341850
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-07-26 02:00:41 +0500

    Initial Commit

---
+++ Calories/CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesApp.swift
+//  Calories
+//
+//  Created by Erkebulan Massainov on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
